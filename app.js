3f37fc852e4f3bc67f98dd6efb460252d1b6b3c4
Munteanu <munteanuvictoria1@gmail.com>
2023-07-03 15:25:30 +0200

    add routing

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("home", {homeStarting: homeStartingContent});
-})
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutStarting: aboutContent});
+});
+
+app.get("/contact",function(req, res){
+  res. render("contact", {contactStarting: contactContent});
+  });
 
 
 
