79897aca78fec320dc234c1b19ec011d9a530cd2
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-07-03 21:37:58 +0200

    add compose page and loop

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts=[];
 
 app.set('view engine', 'ejs');
 
@@ -16,7 +17,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("home", {homeStarting: homeStartingContent});
+  res.render("home", {
+    homeStarting: homeStartingContent,
+    posts:posts });
 });
 
 app.get("/about", function(req, res){
@@ -27,8 +30,18 @@ app.get("/contact",function(req, res){
   res. render("contact", {contactStarting: contactContent});
   });
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+})
 
-
+app.post("/compose", function(req, res){
+  const post={
+    title:req.body.title,
+    content:req.body.content
+  }
+  posts.push(post);
+  res.redirect("/");
+})
 
 
 app.listen(3000, function() {
